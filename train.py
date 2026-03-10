94ff25d5c888e60d30318e8a4bddc0dd6c4d5563
Poojil <185126951+pu3p@users.noreply.github.com>
2026-03-09 22:43:46 -0500

    Revert "Add medical/accounting strong signals for other domain"
    This reverts commit efd3343f0e6f679e2eec7fcb40e0c99d016461ae.

--- train.py
+++ train.py
@@ -372,10 +372,7 @@ def classify_domain(question, choices):
                   "marketing", "customer", "brand",
                   "physical examination", "emergency department", "vital signs",
                   "blood pressure", "likely diagnosis", "most appropriate",
-                  "common stock", "older adults",
-                  "serum", "plasma", "biopsy", "lesion",
-                  "antigen", "antibody", "lymphocyte",
-                  "depreciation", "amortization", "receivable"],
+                  "common stock", "older adults"],
     }
     
     for domain, keywords in DOMAIN_KEYWORDS.items():
