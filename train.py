3956aca378fba084cd2e244498cc14221b0fcfcb
Poojil <185126951+pu3p@users.noreply.github.com>
2026-03-09 22:09:12 -0500

    Adjust word count thresholds (30/17/7)

--- train.py
+++ train.py
@@ -114,11 +114,11 @@ def score_complexity(question, choices):
 
     # Length signal
     word_count = len(question.split())
-    if word_count > 31:
+    if word_count > 30:
         score += 0.18
-    elif word_count > 18:
+    elif word_count > 17:
         score += 0.12
-    elif word_count > 8:
+    elif word_count > 7:
         score += 0.06
 
     # Choice complexity
