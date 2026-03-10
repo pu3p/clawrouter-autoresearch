5b6a278843173edda234cae28c1c56fe52bc7cfc
Poojil <185126951+pu3p@users.noreply.github.com>
2026-03-09 23:27:49 -0500

    Revert "Add STEM strong signals (array, momentum)"
    This reverts commit 9413649edec96513ff41850ea6b4fdbf0753523a.

--- train.py
+++ train.py
@@ -435,7 +435,7 @@ def classify_domain(question, choices):
         "stem": ["theorem", "equation", "algorithm", "molecule", "electron", "integral",
                  "wavelength", "velocity", "compiler", "eigenvalue",
                  "standard deviation", "speed of", "mass of", "energy of",
-                 "work done", "such that", "array", "momentum"],
+                 "work done", "such that"],
         "humanities": ["philosophy", "ethics", "constitution", "fallacy", "theology",
                        "legal", "court", "statute", "morally wrong", "defendant",
                        "archaeological", "prehistoric", "scripture",
